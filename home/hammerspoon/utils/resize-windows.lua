4aea4463e3f8e0a9a9268b478bad724109204b8d
Vitaly Kushner <vitaly@astrails.com>
2016-11-21 18:01:32 +0100

    hammerspoon: fix multydisplay resizing

--- home/hammerspoon/utils/resize-windows.lua
+++ home/hammerspoon/utils/resize-windows.lua
@@ -9,85 +9,97 @@ function resizer(f)
     end
 end
 
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Left", resizer(function(f, max)
-    f.x = max.x
-    f.y = max.y
+function resize_width(max)
+    if (max.w > 3400) then
+        return 0.3
+    else
+        return 0.5
+    end
+end
 
-    f.w = max.w / 2
-    f.h = max.h
-end))
+function resize_height(max)
+    return 0.5
+end
 
+function resize_box(f, max, x, y, w, h)
+    f.x = max.x + x * max.w
+    f.y = max.y + y * max.h
+    f.w = w * max.w
+    f.h = h * max.h
+    hs.alert.show(max)
+end
 
--- right half
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Right", resizer(function(f, max)
-    f.x = max.w / 2
-    f.y = max.y
+resize_left = resizer(function(f, max)
+    w = resize_width(max)
+    resize_box(f, max, 0, 0, w, 1)
+end)
 
-    f.w = max.w / 2
-    f.h = max.h
-end))
+resize_right = resizer(function(f, max)
+    w = resize_width(max)
+    resize_box(f, max, 1 - w, 0, w, 1)
+end)
 
--- top half
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Up", resizer(function(f, max)
-    f.x = max.x
-    f.y = max.y
+resize_up = resizer(function(f, max)
+    h = resize_height(max)
+    resize_box(f, max, 0, 0, 1, h)
+end)
 
-    f.w = max.w
-    f.h = max.h / 2
-end))
+resize_down = resizer(function(f, max)
+    h = resize_height(max)
+    resize_box(f, max, 0, 1 - h, 1, h)
+end)
 
--- top half
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Down", resizer(function(f, max)
-    f.x = max.x
-    f.y = max.h / 2
+resize_up_left = resizer(function(f, max)
+    w = resize_width(max)
+    h = resize_height(max)
+    resize_box(f, max, 0, 0, w, h)
+end)
 
-    f.w = max.w
-    f.h = max.h / 2
-end))
+resize_up_right = resizer(function(f, max)
+    w = resize_width(max)
+    h = resize_height(max)
+    resize_box(f, max, 1 - w, 0, w, h)
+end)
 
--- full screen
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "M", resizer(function(f, max)
-    f.x = max.x
-    f.y = max.y
+resize_down_left = resizer(function(f, max)
+    w = resize_width(max)
+    h = resize_height(max)
+    resize_box(f, max, 0, 1 - h, w, h)
+end)
 
-    f.w = max.w
-    f.h = max.h
-end))
+resize_down_right = resizer(function(f, max)
+    w = resize_width(max)
+    h = resize_height(max)
+    resize_box(f, max, 1 - w, 1 - h, w, h)
+end)
+
+resize_full_screen = resizer(function(f, max)
+    resize_box(f, max, 0, 0, 1, 1)
+end)
+
+-- left
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Left", resize_left)
+
+-- right
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Right", resize_right)
+
+-- top
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Up", resize_up)
+
+-- top
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Down", resize_down)
 
 -- top left
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, ";", resizer(function(f, max)
-    f.x = max.x
-    f.y = max.y
-
-    f.w = max.w / 2
-    f.h = max.h / 2
-end))
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, ";", resize_up_left)
 
 -- top right
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "'", resizer(function(f, max)
-    f.x = max.w / 2
-    f.y = max.y
-
-    f.w = max.w / 2
-    f.h = max.h / 2
-end))
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "'", resize_up_right)
 
 -- bottom left
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, ".", resizer(function(f, max)
-    f.x = max.x
-    f.y = max.h / 2
-
-    f.w = max.w / 2
-    f.h = max.h / 2
-end))
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, ".", resize_down_left)
 
 -- bottom right
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "/", resizer(function(f, max)
-    f.x = max.w / 2
-    f.y = max.h / 2
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "/", resize_down_right)
 
-    f.w = max.w / 2
-    f.h = max.h / 2
-end))
-
-
+-- full screen
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "M", resize_full_screen)
