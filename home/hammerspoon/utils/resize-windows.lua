846df8e414ef9ea6e17569b2a2282d56b2bf2bcc
Vitaly Kushner <vitaly@astrails.com>
2016-11-23 12:50:07 +0100

    hammersoon: middle tiles

--- home/hammerspoon/utils/resize-windows.lua
+++ home/hammerspoon/utils/resize-windows.lua
@@ -2,18 +2,16 @@ function resizer(f)
     return function()
         local w = hs.window.focusedWindow()
         if w then
-            local frame = w:frame()
-            f(frame, w:screen():frame())
-            w:setFrame(frame)
+            w:setFrame(f(w:frame(), w:screen():frame()))
         end
     end
 end
 
 function resize_width(max)
-    if (max.w > 3400) then
-        return 0.3
+    if (max.w > 3000) then
+        return 1/3
     else
-        return 0.5
+        return 1/2
     end
 end
 
@@ -26,80 +24,90 @@ function resize_box(f, max, x, y, w, h)
     f.y = max.y + y * max.h
     f.w = w * max.w
     f.h = h * max.h
-    hs.alert.show(max)
+    return f
 end
 
 resize_left = resizer(function(f, max)
     w = resize_width(max)
-    resize_box(f, max, 0, 0, w, 1)
+    return resize_box(f, max, 0, 0, w, 1)
 end)
 
 resize_right = resizer(function(f, max)
     w = resize_width(max)
-    resize_box(f, max, 1 - w, 0, w, 1)
+    return resize_box(f, max, 1 - w, 0, w, 1)
 end)
 
-resize_up = resizer(function(f, max)
+resize_middle = resizer(function(f, max)
+    w = resize_width(max)
+    return resize_box(f, max, (1 - w)/2, 0, w, 1)
+end)
+
+resize_top = resizer(function(f, max)
     h = resize_height(max)
-    resize_box(f, max, 0, 0, 1, h)
+    return resize_box(f, max, 0, 0, 1, h)
 end)
 
-resize_down = resizer(function(f, max)
+resize_bottom = resizer(function(f, max)
     h = resize_height(max)
-    resize_box(f, max, 0, 1 - h, 1, h)
+    return resize_box(f, max, 0, 1 - h, 1, h)
 end)
 
-resize_up_left = resizer(function(f, max)
+resize_top_left = resizer(function(f, max)
     w = resize_width(max)
     h = resize_height(max)
-    resize_box(f, max, 0, 0, w, h)
+    return resize_box(f, max, 0, 0, w, h)
 end)
 
-resize_up_right = resizer(function(f, max)
+resize_top_middle = resizer(function(f, max)
     w = resize_width(max)
     h = resize_height(max)
-    resize_box(f, max, 1 - w, 0, w, h)
+    return resize_box(f, max, (1 - w)/2, 0, w, h)
 end)
 
-resize_down_left = resizer(function(f, max)
+resize_top_right = resizer(function(f, max)
     w = resize_width(max)
     h = resize_height(max)
-    resize_box(f, max, 0, 1 - h, w, h)
+    return resize_box(f, max, 1 - w, 0, w, h)
 end)
 
-resize_down_right = resizer(function(f, max)
+resize_bottom_left = resizer(function(f, max)
     w = resize_width(max)
     h = resize_height(max)
-    resize_box(f, max, 1 - w, 1 - h, w, h)
+    return resize_box(f, max, 0, 1 - h, w, h)
 end)
+
+resize_bottom_middle = resizer(function(f, max)
+    w = resize_width(max)
+    h = resize_height(max)
+    return resize_box(f, max, (1 - w)/2, 1 - h, w, h)
+end)
+
+resize_bottom_right = resizer(function(f, max)
+    w = resize_width(max)
+    h = resize_height(max)
+    return resize_box(f, max, 1 - w, 1 - h, w, h)
+end)
+
 
 resize_full_screen = resizer(function(f, max)
-    resize_box(f, max, 0, 0, 1, 1)
+    return resize_box(f, max, 0, 0, 1, 1)
 end)
 
--- left
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Left", resize_left)
-
--- right
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Right", resize_right)
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Up", resize_top)
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Down", resize_bottom)
 
--- top
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Up", resize_up)
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "p", resize_top_left)
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "[", resize_top_middle)
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "]", resize_top_right)
 
--- top
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Down", resize_down)
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "l", resize_left)
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, ";", resize_middle)
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "'", resize_right)
 
--- top left
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, ";", resize_up_left)
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, ",", resize_bottom_left)
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, ".", resize_bottom_middle)
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "/", resize_bottom_right)
 
--- top right
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "'", resize_up_right)
-
--- bottom left
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, ".", resize_down_left)
-
--- bottom right
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "/", resize_down_right)
-
--- full screen
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "M", resize_full_screen)
