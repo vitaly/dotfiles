b482da7e971bf25271310ed6cfd8f79d8794fe32
Vitaly Kushner <vitaly@astrails.com>
2018-04-17 04:06:49 +0200

    m

--- home/hammerspoon/utils/hide-apps.lua
+++ home/hammerspoon/utils/hide-apps.lua
@@ -2,7 +2,7 @@ function hideApps()
     local apps = {'Skype', 'Wunderlist', 'Mailplane 3', 'Slack', 'Messages',
     'Spotify', 'Telegram', 'Evernote', 'Sonos', '1Password 6', 'Finder',
     'iTunes', 'Contacts', 'Speed Download', 'Things', 'WhatsApp', 'Tweetbot',
-    'Notes', 'Calendar'}
+    'Notes', 'Calendar', 'HipChat'}
 
     for i, name in ipairs(apps) do
         local app = hs.application.get(name)
